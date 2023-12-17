9626a7a14df470b423cd7367c5ffc4f4af42d8f6
Alexis Jimenez <Alexis.jm29@gmail.com>
2023-12-17 19:39:11 -0400

    fixes

--- frontend_/src/components/sidebar.js
+++ frontend_/src/components/sidebar.js
@@ -37,7 +37,7 @@ const Sidebar = ({togg, func, kind, side, justmobile}) => {
                     <>
                         <form className="search-form flex-center" onSubmit={handleSearch}>
                             <input required type="text" placeholder="Buscar" value={inputValue} className="search-input" onChange={InputChange}></input>
-                            <button type="submit" className="submit-button" style={{display: "flex"}}><FaSearch/></button>
+                            <button type="submit" className="submit-button"><FaSearch/></button>
                         </form>
                         <div className="search-results-container">
                             { results.length > 0 ? results.map((result) => {
